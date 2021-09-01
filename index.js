272fd654d1ee5f3e1a0ca7fb175182928c5efeab
Mit9y4ick <mitygaming@mail.ru>
2021-09-01 16:59:11 +0200

    add comments to login script

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const apiKey = localStorage.getItem('api_key');
+const apiKey = localStorage.getItem('api_key'); // обращение к локальному хранилищу, метод getItem используется для получение значения ключа api_key
 
-if (!apiKey) {
-  const newKey = prompt('Enter API key');
-  if (!newKey) throw new Error('Empty');
-  if (newKey.length !== 31) throw new Error('Too short');
+if (!apiKey) {  // если apiKey имеет значение false
+  const newKey = prompt('Enter API key'); //  выводит в окне браузера окно с полем для ввода ключа 
+  if (!newKey) throw new Error('Empty'); //если значение переменной newKey является false, то появляется ошибка
+  if (newKey.length !== 31) throw new Error('Too short'); //если длина ключа не ровна 31 символам, то появляется ошибка
 
-  localStorage.setItem('api_key', newKey);
+  localStorage.setItem('api_ke  y', newKey); //добавление ключа с именем api_key и значением newKey
 }
 
 console.log('DONE');
