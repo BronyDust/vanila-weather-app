0f0c5f6c117625654904b4eabe0e5581aaac876e
Mit9y4ick <mitygaming@mail.ru>
2021-09-02 16:43:31 +0200

    fixing an error in the code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (!apiKey) {  // если apiKey имеет значение false
   if (!newKey) throw new Error('Empty'); // если значение переменной newKey является false, то появляется ошибка
   if (newKey.length !== 31) throw new Error('Too short'); //если длина ключа не ровна 31 символам, то появляется ошибка
 
-  localStorage.setItem('api_ke  y', newKey); // добавление ключа с именем api_key и значением newKey
+  localStorage.setItem('api_key', newKey); // добавление ключа с именем api_key и значением ключа newKey
 }
 
 console.log('DONE');
