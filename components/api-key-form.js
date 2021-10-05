f249b6f0b11652c133ab0106ae9ea5cdab2daf8f
Mit9y4ick <mitygaming@mail.ru>
2021-10-05 10:28:20 +0200

    correction of errors in the code

--- components/api-key-form.js
+++ components/api-key-form.js
@@ -1,7 +1,8 @@
 class APIKeyForm extends HTMLElement {
-  customElements.define('api-key-form', APIKeyForm)
   constructor() {
     super();
     this.innerHTML = 'api-key-form element';
   }
 }
+
+customElements.define('api-key-form', APIKeyForm)
