735d06b6e0e037976d1666820e5031083e3d53e0
Mit9y4ick <mitygaming@mail.ru>
2021-10-06 10:35:05 +0200

    creating a DOM of a web component

--- components/api-key-form.js
+++ components/api-key-form.js
@@ -1,8 +1,10 @@
 class APIKeyForm extends HTMLElement {
+  #render() {
+    customElements.define('api-key-form', APIKeyForm)
+  }
   constructor() {
     super();
+    let APIKeyForm = document.createElement('api-key-form');
     this.innerHTML = 'api-key-form element';
   }
 }
-
-customElements.define('api-key-form', APIKeyForm)
