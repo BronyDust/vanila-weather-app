3b7d2361906530b20160ff7b866162ee4bc4f8f9
Mit9y4ick <mitygaming@mail.ru>
2021-10-08 21:42:26 +0200

    creating the remaining elements

--- components/api-key-form.js
+++ components/api-key-form.js
@@ -9,6 +9,25 @@ class APIKeyForm extends HTMLElement {
     popupBody.classList.add('popup__body');
     popupRoot.appendChild(popupBody);
 
+    const popupForm = document.createElement('form');
+    popupForm.setAttribute('action', '#');
+    popupForm.setAttribute('method', 'post');
+
+    const popupTitle = document.createElement('h2');
+    popupTitle.classList.add('popup__title');
+
+    const popupInput = document.createElement('input');
+    popupInput.classList.add('popup__input');
+    popupInput.setAttribute('type', 'text');
+    popupInput.setAttribute('text', ' API-ключ');
+    popupInput.setAttribute('pattern', '[A-Za-z0-9]{32}');
+
+    const popupBTNForm = document.createElement('div');
+    popupBTNForm.classList.add('popup__btn-form');
+
+    const popupButton = document.createElement('button');
+    popupButton.classList.add('popup__button');
+
     this.appendChild(popupRoot);
   }
 
