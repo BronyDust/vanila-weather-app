24b095d9012a7837a052e454955f07576d0ba26f
RyanWright <SivanoFFDS@gmail.com>
2021-10-13 10:36:05 +0700

    Connect all nodes to root

--- components/api-key-form.js
+++ components/api-key-form.js
@@ -12,21 +12,26 @@ class APIKeyForm extends HTMLElement {
     const popupForm = document.createElement('form');
     popupForm.setAttribute('action', '#');
     popupForm.setAttribute('method', 'post');
+    popupBody.appendChild(popupForm);
 
     const popupTitle = document.createElement('h2');
     popupTitle.classList.add('popup__title');
+    popupForm.appendChild(popupTitle);
 
     const popupInput = document.createElement('input');
     popupInput.classList.add('popup__input');
     popupInput.setAttribute('type', 'text');
     popupInput.setAttribute('text', ' API-ключ');
     popupInput.setAttribute('pattern', '[A-Za-z0-9]{32}');
+    popupForm.appendChild(popupInput);
 
     const popupBTNForm = document.createElement('div');
     popupBTNForm.classList.add('popup__btn-form');
+    popupForm.appendChild(popupBTNForm);
 
     const popupButton = document.createElement('button');
     popupButton.classList.add('popup__button');
+    popupBTNForm.appendChild(popupButton);
 
     this.appendChild(popupRoot);
   }
