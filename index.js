b4355f7f35fdfa0270fcc665363e3ab5a11dace4
RyanWright <SivanoFFDS@gmail.com>
2021-11-08 20:46:07 +0700

    Connect component to app

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+import APIKeyForm, { SUBMIT_KEY_EVENT_NAME } from './components/api-key-form.js';
 import LocalStorage from './services/LocalStorage.js';
+
+const APIKeyFormComponent = new APIKeyForm();
 
 const apiKeyStorage = new LocalStorage('api_key', function() {
   try {
     const apiKey = this.getValue();
 
     if (!apiKey) {
-      const newKey = prompt('Enter API key');
-      if (!newKey) throw new Error('Empty');
-      if (newKey.length !== 31) throw new Error('Too short');
-
-      this.setValue(newKey);
+      document.body.appendChild(APIKeyFormComponent);
+    } else {
+      APIKeyFormComponent.remove();
     }
   } catch (error) {
     alert(error.message);
     this.clear();
   }
+});
+
+APIKeyFormComponent.addEventListener(SUBMIT_KEY_EVENT_NAME, ({ detail }) => {
+  if (detail) apiKeyStorage.setValue(detail);
 });
 
 const button = document.getElementById('reset');
