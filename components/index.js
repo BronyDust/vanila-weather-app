cd361bcdd01a7271ce6297efb2dfbc08d131d55b
RyanWright <SivanoFFDS@gmail.com>
2021-11-08 20:45:43 +0700

    Remove entry point for custom elements

--- components/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * - Эндпоинт для всех определений web components
- * - Проверка на customElements
- */
-
-if (!('customElements' in window)) throw Error('Браузер не поддерживает customElements! Используйте новые браузеры!');
-
-import './api-key-form.js';
